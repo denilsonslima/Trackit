c1e70fb97402563b71f8522b692f7244a9db6fbb
denilson <denilsondslima@gmail.com>
2023-05-02 02:17:13 -0300

    refactor: reorganize app structure

--- src/App.js
+++ src/App.js
@@ -1,52 +1,46 @@
 import GlobalStyle from "./globalStyles";
-import InitialPage from "./pages/Login";
+import SignIn from "./pages/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginPage from "./pages/Cadastro";
+import SignUp from "./pages/Cadastro";
 import FistPage from "./pages/FistPage";
 import { useEffect } from "react";
 import Hoje from "./pages/Hojepage";
 import Historico from "./pages/Historico";
 import useMyProvider from "./context/context";
+import { RequiredAuth } from "./context/requiredAuth";
 
 function App() {
-  const { token, setToken, setImage, setConcluido } = useMyProvider()
+  const { setToken, setImage } = useMyProvider();
 
-  function setTokenInLocalStorage(dados) {
-    setToken(dados.token);
-    setImage(dados.image)
-    const exemploSerializado = JSON.stringify(dados)
-    localStorage.setItem('lista', exemploSerializado);
+  function setTokenInLocalStorage(data) {
+    setToken(data.token);
+    setImage(data.image);
+    const exemploSerializado = JSON.stringify(data);
+    localStorage.setItem("user", exemploSerializado);
   }
 
-  // function verificar(dados) {
-  //   const total = dados.length
-  //   const feito = dados.filter((e) => e.done === true)
-  //   const porcentagem = Number((feito.length / total) * 100).toFixed(0)
-  //   setConcluido(porcentagem)
-  // }
-
   useEffect(() => {
-    const token = localStorage.getItem('lista');
-    const lista = JSON.parse(token)
-    if (lista) {
-      setToken(lista.token);
-      setImage(lista.image)
+    const data = JSON.parse(localStorage.getItem("user"));
+    if (data) {
+      setToken(data.token);
+      setImage(data.image);
     }
-  }, []);
+  }, [setToken, setImage]);
 
   return (
     <div className="App">
       <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<InitialPage setTokenInLocalStorage={setTokenInLocalStorage} />}></Route>
-          <Route path="/cadastro" element={<LoginPage />}></Route>
+          <Route path="/" element={ <SignIn setTokenInLocalStorage={setTokenInLocalStorage} />}></Route>
+          <Route path="/cadastro" element={<SignUp />}></Route>
           <Route path="/habitos" element={<FistPage />}></Route>
-          <Route path="/hoje" element={<Hoje />}></Route>
+          <Route path="/hoje" element={<RequiredAuth><Hoje /></RequiredAuth>}></Route>
           <Route path="/historico" element={<Historico />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
+
 export default App;
